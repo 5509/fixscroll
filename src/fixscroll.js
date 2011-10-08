beedbc46068f1649cabc34b432e144d1d0c1c06b
Kazunori Tokuda <norimania@gmail.com>
2011-10-08 10:11:24 +0900

    except ie

---
+++ src/fixscroll.js
@@ -0,0 +1,156 @@
+(function(window, document, undefined) {
+
+	window.FixScroll = FixScroll;
+
+	var b = document.body,
+		dE = document.documentElement;
+
+	function FixScroll(elm, options) {
+		var i,
+			self = this;
+
+		if ( self === window ) {
+			return new FixScroll(elm, options);
+		}
+
+		self.opts = {
+			from: 'current',
+			to: 'parent',
+			top: 0,
+			bottom: 0
+		};
+		self.elm = document.getElementById(elm);
+		self.id = elm; // id => id
+		self.state = 'unlocked';
+
+		for ( i in options ) {
+			self.opts[i] = options[i];
+		}
+
+		self._init();
+	}
+
+	FixScroll.prototype = {
+		_init: function() {
+			var self = this;
+			self._getDefault();
+			self._scroll();
+			self._bind();
+		},
+		_getDefault: function() {
+			var self = this,
+				bodyCss,
+				adjustment = null;
+
+			// IE見検証
+			if ( window.getComputedStyle ) {
+				bodyCss = getComputedStyle(b);
+			} else {
+				bodyCss = b.currentStyle;
+			}
+			adjustment = (function() {
+				var _mt = _parseInt(bodyCss['margin-top']),
+					_ml = _parseInt(bodyCss['margin-left']),
+					_pt = _parseInt(bodyCss['padding-top']),
+					_pl = _parseInt(bodyCss['padding-top']);
+				
+				return {
+					top: _mt + _pt,
+					left: _ml + _pl
+				};
+//				return {
+//					top: 0,
+//					left: 0
+//				}
+			}());
+
+			self.defaultPos = {
+				adjTop: adjustment.top,
+				adjLeft: adjustment.left,
+				top: adjustment.top + self.elm.offsetTop,
+				left: adjustment.left + self.elm.offsetLeft
+			};
+		},
+		_setDefault: function() {
+			var self = this;
+			_styles(self.elm, {
+				position: 'static',
+				top: self.defaultPos.top + 'px'//,
+				//left: self.defaultPos.left + 'px'
+			});
+		},
+		_setFix: function() {
+			var self = this;
+			// とりあえずfixed
+			_styles(self.elm, {
+				position: 'fixed',
+				top: self.opts.top + 'px'
+			});
+		},
+		_bind: function() {
+			var self = this;
+			_addEvent(self.elm, self.id + '.locked', function() {
+				console.log('locked');
+				self._setFix();
+			});
+			_addEvent(self.elm, self.id + '.unlocked', function() {
+				console.log('unlocked');
+				self._setDefault();
+			});
+		},
+		_scroll: function() {
+			var self = this;
+			_addEvent(window, 'scroll', function(e) {
+				var scrollTop = b.scrollTop || dE.scrollTop,
+					sumTop = self.defaultPos.top - self.opts.top,
+					borderTop = sumTop < 0 ? 0 : sumTop;
+
+				// triggered
+				if ( scrollTop >= borderTop ) {
+					if ( self.state === 'locked' ) return;
+					self.state = 'locked';
+					_trigger(self.elm, self.id + '.locked')
+				} else {
+					if ( self.state === 'unlocked' ) return;
+					self.state = 'unlocked';
+					_trigger(self.elm, self.id + '.unlocked');
+				}
+			})
+		}
+	};
+
+	function _parseInt(strings) {
+		return parseInt(strings, 10);
+	}
+
+	function _trigger(elm, listener) {
+		var evtObj = undefined;
+		if ( 'fireEvent' in document ) {
+			elm.fireEvent(listener);
+		} else {
+			evtObj = document.createEvent('UIEvents');
+			evtObj.initEvent(listener, false, true);
+			elm.dispatchEvent(evtObj);
+		}
+	}
+
+	function _addEvent(elm, listener, func) {
+		if ( window.addEventListener ) {
+			elm.addEventListener(listener, func, false);
+		} else {
+			if ( listener.indexOf('.') !== -1 ) {
+				elm.attachEvent(listener, func);
+			} else {
+				elm.attachEvent('on' + listener, func);
+			}
+		}
+	}
+
+	function _styles(elm, css) {
+		var i;
+		for ( i in css ) {
+			elm.style[i] = css[i];
+		}
+	}
+
+}(this, this.document));
